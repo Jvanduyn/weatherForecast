58e6f6272bd5e0d737fd756f6235c357baec3703
Jake VanDuyn <108376689+Jvanduyn@users.noreply.github.com>
2022-11-18 10:53:23 -0500

    after working with my tutor. Was able to get the api's to fetch properly.

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,86 @@
-var lon;
-var lat;
 var input = document.querySelector('.searchInput');
 var searchBtn = document.querySelector('.searchBtn');
 var userLocation = document.querySelector('.searchInput').value;
 var prevCities = document.querySelector('.prev-cities');
 
-var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=b2888db4f0baa774a62c34dc4c426cad';
-var geoUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + input + '&limit=1&appid=b2888db4f0baa774a62c34dc4c426cad';
-//how do i get the state code and country code from the input
+var citySearchHistory = []
 
-searchBtn.addEventListener('click', function (event) {
-    event.preventDefault
-});
+function handleUserInput() {
+    var city = input.value
+    getCoord(city)
+}
+function getCoord(city) {
+    var geoUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=b2888db4f0baa774a62c34dc4c426cad';
+    fetch(geoUrl)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            var cityName = data[0].name
+            // console.log(data)
+            saveHistory(cityName)
+            getCurrentWeather(lat, lon);
+            getFutureWeather(lat, lon);
+        })
+}
 
-// need to take the input that the user puts and run it through geo url
-//then need to take the object to get the lat and lon from geo url to put that into requesturl
+function getCurrentWeather(lat, lon) {
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=b2888db4f0baa774a62c34dc4c426cad';
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+
+            console.log(data)
+            //need to obtain name of city, current weather, wind, and humidity and display it on page
+            //create all the elements to display on page
+            //add content to those elements
+            //need to append that content to the elements they need to go into
+        })
+}
+
+function getFutureWeather(lat, lon) {
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=b2888db4f0baa774a62c34dc4c426cad';
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            var forecastArr = [data.list[7], data.list[15], data.list[23], data.list[31], data.list[39]]
+            for (let i = 0; i < forecastArr.length; i++) {
+                //    create elements
+
+                // add content to elements from the forecast arr
+
+                // append newly created elements to the forcast container
+
+            }
+            console.log(forecastArr)
+        })
+}
+
+function saveHistory(city) {
+
+    citySearchHistory.push(city)
+
+    localStorage.setItem('searchHistory', JSON.stringify(citySearchHistory))
+}
+
+
+searchBtn.addEventListener('click', handleUserInput);
+
+
+
 //take the data and display it to the page
 
-//need to do the same thing above for the 5 day weather forcast
 
 //store local storage a list of cities that user has searched
 
-renderLastRegistered();
+// renderLastRegistered();
 
-function renderLastRegistered() {
-    var userLocation = localStorage.getItem("city")
+// function renderLastRegistered() {
+//     var userLocation = localStorage.getItem("city")
 
-    if (!userLocation) {
-        return;
-    }
-
-    prevCities.textContent = userLocation;
-
-};
+//     prevCities.textContent = userLocation;
+//     //need to get teh text as a button
+// };
